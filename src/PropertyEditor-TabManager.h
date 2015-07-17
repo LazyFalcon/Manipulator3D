f9dd0c03e5c25e45730051d29b1e195ab9132348
DezerteR <wajskarol@gmail.com>
2015-07-17 08:40:05 +0200

    init

---
+++ src/PropertyEditor-TabManager.h
@@ -0,0 +1,35 @@
+class ITab;
+class CommandEditorTab;
+class CommandListTab;
+class PathEditorTab;
+class PathListTab;
+class TabManager
+{
+	void clearTab(u32 index){
+		tabs[index].reset();
+	}
+	void initTabs(){
+		if(tabs.size() < 4){
+			tabs.resize(4);
+		}
+		else {
+			if(!tabs[0]) tabs[0] = make_uniqe<CommandEditorTab>();
+			if(!tabs[1]) tabs[1] = make_uniqe<CommandEditorTab>();
+			if(!tabs[2]) tabs[2] = make_uniqe<CommandEditorTab>();
+			if(!tabs[3]) tabs[3] = make_uniqe<CommandEditorTab>();
+		}
+	}
+	void run(){}
+
+	vector<ITab> tabs;
+	u32 currentTab {0};
+};
+
+class ITab
+{
+public:
+	virtual ~ITab(){};
+	virtual void run() = 0;
+};
+
+
