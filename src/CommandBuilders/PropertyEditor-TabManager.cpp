a36f4af21cb89bd588de926a95c7f8e2a81e7217
DezerteR <wajskarol@gmail.com>
2015-07-18 18:46:45 +0200

    up, TM works

--- src/CommandBuilders/PropertyEditor-TabManager.cpp
+++ src/CommandBuilders/PropertyEditor-TabManager.cpp
@@ -37,30 +37,30 @@ void CommandListTab::run(TabManager &TM){
 
 void TabManager::run(){
 
-	// ui.table(UI::LayoutVertical | UI::AlignTop | UI::AlignLeft | UI::Draw)
-		// .style(UI::StyleID::Dark)
-		// .position(screenSize.x-size, screenSize.y-32)
-		// .size(size, -screenSize.y+50);
-	// drawTabs();
-	tabs[currentTab]->run(*this);
+	ui.table(UI::LayoutVertical | UI::AlignTop | UI::AlignLeft | UI::Draw)
+		.style(UI::StyleID::Dark)
+		.position(screenSize.x-size, screenSize.y-32)
+		.size(size, -screenSize.y+32);
+	drawTabs();
+	// tabs[currentTab]->run(*this);
 
 
-	// ui.endTable();
+	ui.endTable();
 }
 
 void TabManager::drawTabs(){
-	// ui.box(UI::LayoutHorizontal);
+	ui.box(UI::LayoutHorizontal);
 
-	// for(u32 i=0; i<tabs.size(); i++){
-		// glm::vec4 rect;
-		// ui.image("Menu-Property-Tab")
-			// .switcher(currentTab, i)
-			// .getRect(rect)
-			// .color(i==currentTab ? 0xB77F3FFF : 0x808080FF)
-			// (UI::Button);
-	// }
+	for(u32 i=0; i<tabs.size(); i++){
+		glm::vec4 rect;
+		ui.image("Menu-Property-Tab")
+			.switcher(currentTab, i)
+			.getRect(rect)
+			.color(i==currentTab ? 0xB77F3FFF : 0x808080FF)
+			(UI::Button);
+	}
 
-	// ui.endBox();
+	ui.endBox();
 }
 
 
