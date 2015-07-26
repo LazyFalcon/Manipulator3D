4ac122bd1b361592ed075ff87b65a37d20e3dbc3
DezerteR <wajskarol@gmail.com>
2015-07-26 09:13:34 +0200

    new SSAO variation

--- shaders/SSAO.glsl
+++ shaders/SSAO.glsl
@@ -27,11 +27,11 @@ in vec2 vUV;
 // const float bias = -0.169;
 // const float scale = 50.01;
 // const float intensity = 150.1;
-const float bias = -0.069;
+const float bias = -0.0069;
 const float scale = 10.01;
 const float intensity = 3.1;
 // const vec2 sampleRadius = 95/vec2(1400, 720);
-const vec2 sampleRadius = 15/vec2(1400, 720);
+const vec2 sampleRadius = 20/vec2(1400, 720);
 const float randomSize = 60;
 
 
@@ -50,36 +50,18 @@ vec4 getPosition(vec2 uv, out float depth){
 
 vec2 getRandom(vec2 uv){
 	return normalize(texture(uSSAORandom, uv*randomSize)).xy;
-	// return normalize(texture(uSSAORandom, uv*randomSize)).xy*2 - vec2(1);
 }
-/*
+
 float computeAmbientOcclusion(vec2 uv, vec2 dCoord, vec4 position, vec4 normal){
 	float depth;
 	vec4 diff = getPosition(uv + dCoord, depth) - position;
 	float d = length(diff);
 	vec4 v = diff / d;
-	// return clamp(max(0.0, dot(normal,v)-bias) * (1.0/(1.0 + d*scale)) * intensity, 0, 1);
-	return max(0.0, dot(normal,v)-bias) * (1.0/(1.0 + d*scale)) * intensity;
-} */
-float computeAmbientOcclusion(vec2 uv, vec2 dCoord, vec4 position, vec4 normal){
-	float depth;
-	vec4 diff = getPosition(uv + dCoord, depth) - position;
-	float d = length(diff);
-	vec4 v = diff / d;
-	return max(0.0, dot(normal,v)-bias) * (1.0/(1.0 + d*scale)) * intensity;
+	return max(-0.50, dot(normal,v)-bias) * (1.0/(1.0 + d*scale)) * intensity;
 }
 
 vec2 kernel[16] = vec2[]
 (
-	// vec2(1,1),
-	// vec2(-1,-1),
-	// vec2(-1,1),
-	// vec2(1,-1)
-	// vec2(1,0),
-	// vec2(0,-1),
-	// vec2(-1,0),
-	// vec2(0,1)
-
 	vec2( -0.94201624,  -0.39906216 ),
 	vec2(  0.94558609,  -0.76890725 ),
 	vec2( -0.094184101, -0.92938870 ),
@@ -103,24 +85,23 @@ void main(void){
 	float depth;
 	vec4 position = getPosition(vUV, depth);
 	vec4 normal = getNormal(vUV);
-	// vec2 rand = getRandom(vUV);
-	vec2 rand = getRandom(position.xz*vUV);
-	// vec2 rand = getRandom(normal.xz);
+	// vec2 rand = getRandom(position.xz*vUV);
 
 	float ao = 0.0;
-	vec2 radius = max(sampleRadius, sampleRadius/(1 - depth*2.9));
-
-	// float radius = sampleRadius*depth*1.0;
-	// float radius = sampleRadius.x;
+	vec2 radius = min(sampleRadius, sampleRadius/(1 - depth*1.9));
+	// vec2 radius = sampleRadius/(1 - depth*2.9);
 
 	const int iterations = 16;
 	for(int i=0; i<iterations; ++i){
+		vec2 rand = getRandom(position.xz*vUV*i);
 		// vec2 refl = reflect(kernel[i], rand) * radius;
 		vec2 refl = kernel[i] * radius;
-		ao += computeAmbientOcclusion(vUV, refl, position, normal);
+		ao += computeAmbientOcclusion(vUV, refl*rand, position, normal);
 	}
-	ao = clamp(ao/1.0/float(iterations), 0, 2);
+	// ao *= ao;
+	ao = clamp(ao/float(iterations), 0, 2);
 	ao = sqrt(ao);
+	// ao = ceil(ao*6)/6;
 	// outAO = clamp(vec4(1.0-ao/4.0/float(iterations)*28*depth), 0.5, 2);
 	// outAO = vec4(1.0-ao/4.0/float(iterations)*28*depth);
 	outAO = vec4(1.3-ao);
