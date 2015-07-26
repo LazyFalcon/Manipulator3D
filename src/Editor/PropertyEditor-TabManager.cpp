c609a7990af70831ba3ebac8f966ae4676079ee3
DezerteR <wajskarol@gmail.com>
2015-07-26 23:42:38 +0200

    up

--- src/Editor/PropertyEditor-TabManager.cpp
+++ src/Editor/PropertyEditor-TabManager.cpp
@@ -9,6 +9,57 @@ extern UI::IMGUI ui;
 extern glm::vec2 screenSize;
 namespace Editor NAM_START
 
+/// ----- TAB MANAGER ---------------------------------------------
+void TabManager::run(){
+
+	ui.table(UI::LayoutVertical | UI::AlignTop | UI::AlignLeft | UI::Draw)
+		.style(UI::StyleID::Dark)
+		.position(screenSize.x-size, screenSize.y-32)
+		.size(size, -screenSize.y+32);
+	drawTabs();
+
+	tabs[currentTab]->run(*this);
+
+	ui.endTable();
+}
+
+void TabManager::drawTabs(){
+	ui.box(UI::LayoutHorizontal);
+
+	// for(u32 i=0; i<tabs.size(); i++){
+	for(u32 i=0; i<4; i++){
+		glm::vec4 rect;
+		ui.image("Menu-Property-Tab")
+			.switcher(currentTab, i)
+			.getRect(rect)
+			.color(i==currentTab ? 0xB77F3FFF : 0x808080FF)
+			.onlClick([i, this]{
+				setTab(i);
+			})
+			(UI::Button);
+	}
+
+	ui.endBox();
+}
+
+template<>
+CommandEditorTab& TabManager::get<CommandEditorTab>(){
+	return static_cast<CommandEditorTab&>(*tabs[0]);
+}
+template<>
+CommandListTab& TabManager::get<CommandListTab>(){
+	return static_cast<CommandListTab&>(*tabs[1]);
+}
+template<>
+PathEditorTab& TabManager::get<PathEditorTab>(){
+	return static_cast<PathEditorTab&>(*tabs[2]);
+}
+template<>
+PathListTab& TabManager::get<PathListTab>(){
+	return static_cast<PathListTab&>(*tabs[3]);
+}
+
+
 extern PolylineEditor polylineEditor;
 unique_ptr<ICommandBuilderWidget> createWidgetFromCommandAndSetPtr(shared_ptr<ICommand> &ptr){
 	unique_ptr<ICommandBuilderWidget> out;
@@ -63,62 +114,10 @@ void PathEditorTab::run(TabManager &TM){
 void PathListTab::run(TabManager &TM){
 	for(auto &it : getInterpolators()){
 		ui.rect(150, 20).text(it->name)();
-		ui.rect(150, 1).color(0xFFFFFFFF)(UI::Label);
-			// .onrClick(TM.get<CommandEditorTab>().setToEdit(it));
+		ui.rect(150, 1).color(0xFFFFFFFF)(UI::Label)
+			.onlClick([&TM, &it]{TM.get<PathEditorTab>().reset(it);});
 	}
 
-}
-
-
-
-/// ----- TAB MANAGER ---------------------------------------------
-void TabManager::run(){
-
-	ui.table(UI::LayoutVertical | UI::AlignTop | UI::AlignLeft | UI::Draw)
-		.style(UI::StyleID::Dark)
-		.position(screenSize.x-size, screenSize.y-32)
-		.size(size, -screenSize.y+32);
-	drawTabs();
-
-	tabs[currentTab]->run(*this);
-
-	ui.endTable();
-}
-
-void TabManager::drawTabs(){
-	ui.box(UI::LayoutHorizontal);
-
-	// for(u32 i=0; i<tabs.size(); i++){
-	for(u32 i=0; i<4; i++){
-		glm::vec4 rect;
-		ui.image("Menu-Property-Tab")
-			.switcher(currentTab, i)
-			.getRect(rect)
-			.color(i==currentTab ? 0xB77F3FFF : 0x808080FF)
-			.onlClick([i, this]{
-				setTab(i);
-			})
-			(UI::Button);
-	}
-
-	ui.endBox();
-}
-
-template<>
-CommandEditorTab& TabManager::get<CommandEditorTab>(){
-	return static_cast<CommandEditorTab&>(*tabs[0]);
-}
-template<>
-CommandListTab& TabManager::get<CommandListTab>(){
-	return static_cast<CommandListTab&>(*tabs[1]);
-}
-template<>
-PathEditorTab& TabManager::get<PathEditorTab>(){
-	return static_cast<PathEditorTab&>(*tabs[2]);
-}
-template<>
-PathListTab& TabManager::get<PathListTab>(){
-	return static_cast<PathListTab&>(*tabs[3]);
 }
 
 
