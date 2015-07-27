4890ce3bed775db5096bbcde64be7e6f333c6062
DezerteR <wajskarol@gmail.com>
2015-07-27 14:02:57 +0200

    removed moveCommand implementation

--- src/RobotController.cpp
+++ src/RobotController.cpp
@@ -160,53 +160,7 @@ bool RobotController::update(float dt){
 	return false;
 }
 
-void MoveCommand::init(RobotController &rc){
-	isRuning = true;
-	solver->solve(Point{ interpolator->firstPoint(), glm::quat(0, 0, 0, 1) }, *rc.robot);
-	targetJointPosition = solver->result;
-	rc.robot->isReady = false;
-	std::cout<<"Init command"<<std::endl;
 
-
-	previousPoint = rc.robot->endEffector.position;
-	rc.robot->insertVariables(targetJointPosition);
-}
-double MoveCommand::calculateRequiredDistance(float dt){
-	return dt*velocity;
-}
-glm::vec4 MoveCommand::calculateNextPoint(float dt){
-	requiredDistance = calculateRequiredDistance(dt);
-
-	glm::vec4 newTarget;
-
-	while(requiredDistance > 0.0 && (not interpolator->finished)){
-		newTarget = interpolator->nextPoint();
-		requiredDistance -= glm::distance(previousPoint, newTarget);
-		previousPoint = newTarget;
-	}
-
-	return newTarget;
-}
-bool MoveCommand::update(RobotController &rc, float dt){
-
-	if(not rc.robot->isReady){
-		rc.robot->goTo(dt);
-		previousPoint = rc.robot->endEffector.position;
-		return false;
-	}
-	if(rc.robot->isReady && interpolator->finished){
-		interpolator->reset();
-		std::cout<<"job done"<<endl;
-		return false;
-	}
-	glm::vec4 newTarget = calculateNextPoint(dt);
-
-	solver->solve(Point{ newTarget, glm::quat(0, 0, 0, 1) }, *rc.robot);
-	targetJointPosition = solver->result;
-	rc.robot->goTo(targetJointPosition);
-
-	return false;
-}
 
 
 
